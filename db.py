b59fab119d0c7caa9a2d1fb4659627ec70710ea0
pratyushz0 <pratyushkatel@gmail.com>
2026-05-23 09:08:57 +0545

    db intialization:

--- db.py
+++ db.py
@@ -8,9 +8,17 @@ chroma_client = chromadb.PersistentClient(db_path , )
 print(base_dir)
 
 
-def store_vectors ( embeddings ) : 
+def store_vectors ( embeddings ,ids) : 
     """
     Model interface to storee the generated embeddings onto the vector database for future retrieval 
     and Augmented Generations from it :)
     """
+    collections = chroma_client.get_or_create_collection(name = "document_embeddings")
 
+    collections.add(
+        embeddings=embeddings,
+        ids=ids
+    )
+    print(f"embedding has been store d in the database")
+
+store_vectors([12,32,4123,331],"101")
